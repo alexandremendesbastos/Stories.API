4fb5bc6aa3d18d50b0abd12aa113040b96c0c03b
Alexandre Mendes Bastos <alexandrem.bastos@gmail.com>
2024-02-19 14:53:09 -0300

    Backend funcional

--- Stories.API/Program.cs
+++ Stories.API/Program.cs
@@ -7,7 +7,6 @@ using Stories.API.Service.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
